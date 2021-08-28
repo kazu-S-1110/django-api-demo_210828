09704344fd9ff7432cfb0301d6c7c8d42fc19ec2
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-28 23:54:51 +0900

    Bookオブジェクトを更新するためのViewSetを定義。

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from shop.models import Book
+from .serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializers = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
